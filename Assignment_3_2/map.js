daa646abb98a4e1174939d7961aa97c50289fe4e
shanelovesmochi <122473578+shanelovesmochi@users.noreply.github.com>
2025-04-30 15:52:06 -0400

    Update map.js

--- Assignment_3_2/map.js
+++ Assignment_3_2/map.js
@@ -5,9 +5,9 @@ window.onload = function () {
         container: 'map',
         style: 'mapbox://styles/zhuxiaofan/cm6jjln1800bt01qqcqamed19',
         center: [-73.935242, 40.730610], // NYC
-        zoom: 10,
-        maxZoom: 15,
-        minZoom: 4,
+        zoom: 3,
+        maxZoom: 6,
+        minZoom: 2,
         projection: 'mercator'
     });
 
@@ -19,7 +19,7 @@ document.getElementById('projectionSelect').addEventListener('change', function 
 
 
 map.on('load', function () {
-    fetch("data/Age_Census_merged_data.geojson")
+    fetch("data/Merged_World_Simplify.geojson")
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -49,16 +49,16 @@ map.on('load', function () {
             source: 'census_data',
             paint: {
                 'fill-color': [
-                    'step', ['get', 'Total Percent age 60 above'],
+                    'step', ['get', 'PERCENTAGE'],
                     '#f1b481',
-                    16.3, '#df6c62',
-                    24.5, '#a84178',
-                    33.5, '#6b237c',
-                    52.7, '#250c4d',
+                    5.7, '#df6c62',
+                    11.1, '#a84178',
+                    17.5, '#6b237c',
+                    29.5, '#250c4d',
                 ],
                 'fill-opacity': [
                     'case',
-                    ['==', ['get', 'Total Percent age 60 above'], 0], 0.1,  // If 0, almost transparent
+                    ['==', ['get', 'PERCENTAGE'], 0], 0.1,  // If 0, almost transparent
                     0.9  // Default opacity for all other values
                 ],
                 'fill-outline-color': '#ffffff'
@@ -72,9 +72,9 @@ map.on('load', function () {
             new mapboxgl.Popup()
                 .setLngLat(e.lngLat)
                 .setHTML(`
-                    <h4>Census Tract: ${props.GEOID}</h4>
-                    <p>Total Population: ${props["Total population"] || "N/A"}</p>
-                    <p>Percent Age 60 Above: ${props["Total Percent age 60 above"] || "N/A"}%</p>
+                    <h4>Country: ${props["NAME"]}</h4>
+                    <p>Total Population: ${props["NUM"] || "N/A"}</p>
+                    <p>Percent Age 60 Above: ${props["PERCENTAGE"] || "N/A"}%</p>
                 `)
                 .addTo(map);
         });
